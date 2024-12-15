bc862149f6aec99a57d41bb966658e6e8e96197d
yehorgorstein <gorstein.yehor@gmail.com>
2024-12-15 14:26:10 +0100

    Gjort cells i griden

--- common.js
+++ common.js
@@ -1,14 +1,32 @@
-function numberRandomizer (){
-    return 1 + Math.floor((99 + 1- 1) * Math.random());
+const numberRandomizer = () => {
+    return 1 + Math.floor(99 * Math.random());
 }
+
+let grid = document.createElement("div");
+document.body.appendChild(grid);
+grid.style.display = "flex";
+grid.style.gap = "5px";
+grid.style.flexWrap = "wrap";
 
 const createCells = amountOfCells => {
-    let grid = document.createElement("div");
-    document.body.appendChild(grid);
+    grid.innerHTML = "";
+    for (let i = 0; i < amountOfCells; i++){
+        let cell = document.createElement("div");
+        cell.textContent = numberRandomizer();
+        cell.style.backgroundColor = "lightgrey";
+        cell.style.height = "50px";
+        cell.style.width = "50px";
+        cell.style.borderRadius = "10px";
+        cell.style.display = "flex";
+        cell.style.alignItems = "center";
+        cell.style.justifyContent = "center";
+        grid.appendChild(cell);
+    }
+};
 
-    let buttonCreate = document.getElementById("buttonCreate");
-    buttonCreate.addEventListener("click", function(){
-    let cell = document.createElement("div");
-    grid.appendChild(cell);
+let chooseAmount = document.getElementById("chooseAmount");
+let createButton = document.getElementById("buttonCreate");
+
+createButton.addEventListener("click", function(){
+    createCells(chooseAmount.value)
 });
-}
