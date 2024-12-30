6d709ad92a0af63da9d9b644525fa89d00a519cf
yehorgorstein <gorstein.yehor@gmail.com>
2024-12-30 16:12:53 +0100

    Byggt färdigt clear filen

--- clear/index.js
+++ clear/index.js
@@ -1,7 +1,29 @@
 let fillCleared = document.getElementById("fillCleared");
 
 fillCleared.addEventListener("click", function(){
-    for (cell of grid.children){
-        cell.style.backgroundColor = "lightgrey";
+    for (let cell of grid.children){
+        cell.classList.remove("cellCleared");
+        if (cell.textContent == ""){
+            cell.textContent = numberRandomizer();
+        }
     }
 })
+
+const clearCells = () => {
+    for (let cell of grid.children){
+        cell.addEventListener("click", function(){
+            if (cell.classList.contains("cellCleared")){
+                cell.classList.remove("cellCleared");
+                cell.textContent = numberRandomizer();
+            } else {
+                cell.classList.add("cellCleared");
+                cell.textContent = "";
+            }
+        })
+    }
+}
+
+createButton.addEventListener("click", function(){
+    clearCells();
+})
+clearCells();
