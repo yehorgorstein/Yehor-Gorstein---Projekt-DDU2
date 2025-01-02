c4a3ba79180197744ec56ca41a82004de4fdea71
yehorgorstein <gorstein.yehor@gmail.com>
2025-01-02 12:57:40 +0100

    Gjort klart sum filen

--- sum/index.js
+++ sum/index.js
@@ -1,34 +1,45 @@
 let sumAllInput = document.getElementById("sumAllInput");
 let sumMarkedInput = document.getElementById("sumMarkedInput");
-sumMarkedInput.value = "-";
+let buttonReset = document.getElementById("buttonReset");
 
 const sumAll = () => {
     totalSum = 0;
     for (let cell of grid.children){
         let cellValue = Number(cell.textContent);
-        if (cell){
+        if (cellValue){
             totalSum += cellValue;
         }
     }
     sumAllInput.value = totalSum;
 }
 
-
 const sumMarked = () => {
+    sumMarkedInput.value = "-";
     for (let cell of grid.children){
         cell.addEventListener("click", () => {
             let cellValue = Number(cell.textContent);
             cell.style.backgroundColor = "lightgreen";
-            if (cell){
-                sumMarkedInput.value += cellValue;
+            if (cellValue){
+                let currentValue = Number(sumMarkedInput.value);
+                if (isNaN(currentValue)){
+                    currentValue = 0;
+                }
+                sumMarkedInput.value = currentValue + cellValue;
             }
         })
     }
 }
 
+createButton.addEventListener("click", () => {
+    sumAll();
+    sumMarked();
+})
 sumAll();
 sumMarked();
 
-createButton.addEventListener("click", () => {
-    sumAll();
+buttonReset.addEventListener("click", () => {
+    sumMarkedInput.value = "-";
+    for (let cell of grid.children){
+        cell.style.backgroundColor = "lightgrey";
+    }
 })
